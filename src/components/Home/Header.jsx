fd76caf76fda15a8452233a773f347a536674814
Said00510 <dylanrivas200206@gmail.com>
2023-11-05 10:25:58 -0500

    Arregle un bug en el header de la carpeta home en diseño responsive

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -17,7 +17,7 @@ function HomeHeader() {
             <span className="custom-font-dancing uppercase font-bold text-[15px] xsm:text-lg tracking-widest mb-0 xsm:mb-2 hidden xsm:block">
               Apasionada por aprender
             </span>
-            <div className="flex gap-12 flex-col w-[90%] ">
+            <div className="flex gap-2 xsm:gap-4 md:gap-12 flex-col w-[90%] ">
               <h1 className="font-bold text-[28px] xsm:text-5xl uppercase">
                 Ing. Industrial
               </h1>
@@ -28,9 +28,9 @@ function HomeHeader() {
                 eficiencia y la eficacia en sus operaciones.
               </p>
 
-              <button className="btn button lg:rotate-[-10deg] inline-flex w-max px-3 py-2 xsm:px-7 xsm:py-3">
+              <button className="btn button text-[12px] xsm:text-[18px] md:text-[20px] lg:rotate-[-10deg] inline-flex w-max px-3 py-2 xsm:px-7 xsm:py-3">
                 <a href="../../../public/downloads/CV ANGIE MABEL RIVAS GUERRERO-1.pdf" download>
-                  <span className="text-xl">DESCARGAR CV</span>
+                  <span className="text-[12px] xsm:text-xl">DESCARGAR CV</span>
                 </a>
               </button>
             </div>
